3f7484895e2e5639267abc4633b5451f6c5cad5c
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-12-29 12:59:59 +0700

    add: Numbers

---
+++ bin/5.numbers.dart
@@ -0,0 +1,23 @@
+// Numbers
+
+// int
+var number = 2020;
+var hex = 0xDEADBEEF;
+
+// double
+var decimal = 1.2;
+var pi = 3.14;
+double withoutDecimal = 7; // same as 7.0
+
+// convert
+// String -> int
+var eleven = int.parse('11'); // 11
+
+// String -> double
+var elevenPointTwo = double.parse('11.2'); // 11.2
+
+// int -> String
+var elevenAsString = 11.toString(); // '11'
+
+// double -> String
+var piAsString = 3.14159.toStringAsFixed(2); // '3.14'
