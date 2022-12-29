3c65739fe3964cd5cca03825f8c5c67ab7acddac
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-12-29 13:20:06 +0700

    add: Booleans

---
+++ bin/7.booleans.dart
@@ -0,0 +1,17 @@
+// Booleans
+
+var alwaysTrue = true;
+var alwaysFalse = false;
+
+// Not or Bang (!)
+var notTrue = !true;
+var notFalse = !false;
+
+// Sample
+void main() {
+  if (true) {
+    print("It's True");
+  } else {
+    print("It's False");
+  }
+}
