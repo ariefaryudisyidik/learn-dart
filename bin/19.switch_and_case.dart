0b67e55e734d2969c6ec47c5ff77c3562f25f76e
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-12-31 06:59:41 +0700

    add: Switch and Case

---
+++ bin/19.switch_and_case.dart
@@ -0,0 +1,33 @@
+// Switch and Case
+
+import 'dart:io';
+
+void main() {
+  stdout.write('Input first number: ');
+  var firstNumber = num.parse(stdin.readLineSync()!);
+  stdout.write('Input operator [ + | - | * | / ]: ');
+  var operator = stdin.readLineSync();
+  stdout.write('Input second number: ');
+  var secondNumber = num.parse(stdin.readLineSync()!);
+
+  switch (operator) {
+    case '+':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber + secondNumber}');
+      break;
+    case '-':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber - secondNumber}');
+      break;
+    case '*':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber * secondNumber}');
+      break;
+    case '/':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber / secondNumber}');
+      break;
+    default:
+      print('Operators not found');
+  }
+}
