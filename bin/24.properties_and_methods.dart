ea245297d4d4ebac8f3b7d900a7748fda96b2848
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-12-31 21:45:21 +0700

    add: Properties and Methods

---
+++ bin/24.properties_and_methods.dart
@@ -0,0 +1,11 @@
+// Properties and Method
+import '24.Animal.dart';
+
+void main() {
+  var dicodingCat = Animal('Gray', 2, 4.2);
+
+  dicodingCat.eat();
+  dicodingCat.sleep();
+  dicodingCat.poop();
+  print(dicodingCat.weight);
+}
