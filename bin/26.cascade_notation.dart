519bbce86c6bbf30609904cd5d45f3ac351e383f
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-12-31 22:12:01 +0700

    add: Cascade Notation

---
+++ bin/26.cascade_notation.dart
@@ -0,0 +1,23 @@
+// Cascade Notation
+
+import '24.Animal.dart';
+
+void main() {
+  // Cascade Notation (.. or ?..)
+  var dicodingCat = Animal('', 2, 4.2)
+    ..name = 'Gray'
+    ..eat(); // Output: Gray is eating.
+
+  // Same with this
+  // dicodingCat.name = 'Gray';
+  // dicodingCat.eat();
+}
+
+void cascadeSample() {
+  // We will also often encounter cascade notation in builder patterns
+  final addressBook = (AddressBookBuilder()
+    ..name = 'jenny'
+    ..email = 'jenny@example.com'
+    ..phone = '415-555-0100')
+      .build()
+}
