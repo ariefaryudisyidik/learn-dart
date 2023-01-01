f8cdfeac52a338b3546d350b1ab87aa3a0458701
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2023-01-01 10:41:00 +0700

    add: Functional Programming

---
+++ bin/33.functional_programming.dart
@@ -0,0 +1,23 @@
+// Functional Programming Paradigm
+
+// Pure Functions
+int sum(int num1, int num2) {
+  return num1 + num2;
+}
+
+// Recursion
+int fibonacci(n) {
+  if (n <= 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+void main() {
+  // Immutable Variables
+  var x = 5;
+  x = x + 1; // Sample mutable variable
+}
