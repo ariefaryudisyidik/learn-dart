51dd5052f80ffc64844bf251d06c58c5c43ee343
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2023-01-01 09:53:18 +0700

    add: Extension Methods

---
+++ bin/32.extension_methods.dart
@@ -0,0 +1,10 @@
+// Extension Methods
+
+import '32.extensions.dart';
+
+void main() {
+  var unsortedNumbers = [2, 5, 3, 1, 4];
+  print(unsortedNumbers);
+  var sortedNumbers = unsortedNumbers.sortAsc();
+  print(unsortedNumbers);
+}
